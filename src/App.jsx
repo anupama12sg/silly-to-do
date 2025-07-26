286f13be22353505531610035be99f802359b72f
Aditya <anupama12sg@gmail.com>
2025-07-26 19:33:18 +0530

    chore: bg color change

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React from 'react'
+import Todo from './components/Todo'
 
 const App = () => {
   return (
-    <div>
-      
+    <div className='bg-fuchsia-500 grid py-4 min-h-screen  '>
+      <Todo/>
     </div>
   )
 }
