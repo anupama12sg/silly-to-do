f43de2cc63258adee725ab84e6ddb76d21cf15f7
Aditya <anupama12sg@gmail.com>
2025-07-26 19:59:32 +0530

    chore: created h1 and img & added some design changes to it

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import todo from '../assets/todo.webp'
 
 const Todo = () => {
     return (
         <div className='bg-white place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-[550px] rounded-xl'>
-            
+
+            {/*------title------ */}
+            <div className='flex items-center mt-7 gap-2'>
+                <img className='w-8' src={todo} alt='' />
+                <h1 className='text-3xl font-semibold'>To-Do List</h1>
+            </div>
+
         </div>
     )
 }
