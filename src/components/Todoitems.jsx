8ca937d8b9b733716e543c07ee392600a1cce0c5
Aditya <anupama12sg@gmail.com>
2025-07-27 19:10:35 +0530

    chore: imported icons to Todoitems component, created div & p

--- src/components/Todoitems.jsx
+++ src/components/Todoitems.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import tick from '../assets/tick/webp'
+import not_tick from '../assets/not_tick.webp'
+import delete_icon from '../assets/delete_icon.webp'
 
 const Todoitems = () => {
     return (
         <div className='flex items-center my-3 gap-2'>
             <div>
-
+                <img src={tick} alt="" />
+                <p>Learn Coding</p>
             </div>
         </div>
     )
