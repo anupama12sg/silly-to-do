be8dc0fa52f99dd09b3e2723007be6aff72cb658
Aditya <anupama12sg@gmail.com>
2025-08-17 17:08:59 +0530

    chore: added line through to completed tasks

--- src/components/Todoitems.jsx
+++ src/components/Todoitems.jsx
@@ -8,7 +8,7 @@ const Todoitems = ({ text, id, isComplete, deleteTodo, toggle }) => {
         <div onClick={() => { toggle(id) }} className='flex items-center my-3 gap-2'>
             <div className='flex flex-1 items-center cursor-pointer'>
                 <img src={isComplete ? tick_icon : not_tick} alt="" className='w-7' />
-                <p className='text-slate-700 ml-4 text-[17px]'>{text}</p>
+                <p className={`text-slate-700 ml-4 text-[17px] decoration-slate-500 ${isComplete ? "line-through" : ""}`}>{text}</p>
             </div>
 
             <img onClick={() => { deleteTodo(id) }} src={delete_icon} alt="" className='w-4 cursor-pointer' />
